2305fa1bf577d21c346b13961b5f91ea392258bd
Kevmsutton <kevmsutton@gmail.com>
2019-12-07 10:05:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(str) {
+    str = str.split(' ');
+
+    for (let i = 0; i < str.length; i++) {
+      str[i] = str[i][0].toUpperCase() + str[i].slice(1);
+    }
+
+    return str.join(' ');
+  }
 }
